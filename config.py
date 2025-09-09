8c6efa3d90dfd56e09b7514d6572f5977147b0e1
23f3000733 <140433073+23f3000733@users.noreply.github.com>
2025-09-09 07:26:36 +0000

    Fix database URI in Config class to use the correct Render environment URL

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
     # Prefer DATABASE_URL from Render env, fallback to your manual Postgres connection
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL") or
-        "postgresql://parkease_data_user:wE23Fbe1QWSzBPSU2GSLeMFyXgZdXapa@dpg
+        "postgresql://parkease_data_user:wE23Fbe1QWSzBPSU2GSLeMFyXgZdXapa@dpg-d2vt87idbo4c73b1id1g-a.oregon-postgres.render.com/parkease_data"
