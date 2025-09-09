2c0121d963b81aa784dc89e0ec291b74ae56c030
23f3000733 <140433073+23f3000733@users.noreply.github.com>
2025-09-09 16:05:36 +0000

    Implement initial database migration with tables for parking lots, users, parking spots, and reservations

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
 from flask import Flask, render_template
-from models import db
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import Config
-
+from models import db, User
 from admin import admin_bp
 from auth import auth_bp
 from user import user_bp
+from datetime import datetime, timedelta
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate = Migrate(app, db) 
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from models import User
-
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-
-    from datetime import datetime, timedelta
 
     @app.context_processor
     def inject_datetime():
@@ -38,14 +36,6 @@ def create_app():
     def home():
         return render_template('hero.html')
 
-    # ✅ Ensure tables exist (for Render too)
-    with app.app_context():
-        db.create_all()
-
     return app
 
-
 app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
